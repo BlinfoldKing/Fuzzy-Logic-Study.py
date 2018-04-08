f1f4ec79996531ef40a20bc8f1b4828485a25f16
Ganesha Danu <ganeshadanu1@gmail.com>
2018-04-09 01:18:10 +0700

    add Fuzzification and Interference

--- opRecEvaluator.py
+++ opRecEvaluator.py
@@ -5,53 +5,80 @@ import math
 
 workbook = xlrd.open_workbook("Dataset.xlsx")
 dataset = workbook.sheet_by_index(0)
+controlData = []
 testData = []
+
 
 #thresshold
 
-high = 70
-mid  = 50
-low  = 25
+mid  = 70
+low  = 30
+
 interval = 5
 
+def dist (a, b):
+    return b - a
 
-def sigmoidUp (x, a, b, c):
-    if (x <= a):
+def sigmoidUp (x, a, b):
+    if x <= a:
         return 0
-    elif (x <= b):
-        return 2 * math.pow((x - a) / (c-a), 2)
-    elif (x < c):
-        return 1 - (2 * math.pow((c - x) / (c - a)))
-    else
+    elif x <= a + dist(a, b) / 2:
+        return 2 * math.pow((x - a) / (b-a), 2)
+    elif x < b:
+        return 1 - (2 * math.pow((b - x) / (b - a), 2))
+    else:
         return 1
 
-def sigmoiDown (x, a, b, c):
-    if (x <= a):
+def sigmoidDown (x, a, b):
+    if x <= a:
         return 1
-    elif (x <= b):
-        return 1 - (2 * math.pow((c - x) / (c - a)))
-    elif (x < c):
-        return 2 * math.pow((x - a) / (c-a), 2)
+    elif x <= a + dist(a, b) / 2:
+        return 1 - (2 * math.pow((x - a) / (b - a), 2))
+    elif x < b:
+        return 2 * math.pow((b - x) / (b - a), 2)
     else:
         return 0
 
-
+def sigmoid (x, min, max):
+    return sigmoidUp(x, min, min + (dist(min, max) / 2)) if (x < min + dist(min, max) / 2) else sigmoidDown(x, min + (dist(min, max) / 2), max) 
 
 def rendah (x):
-    pass
+    return sigmoidDown (x, 0, low + interval)
 
 def sedang (x):
-    pass
+    return sigmoid (x, low - interval, mid + interval)
 
 def tinggi (x):
-    pass
+    return sigmoidUp (x, mid - interval, 100)
 
+def Fuzzification (crispData):
+    return {
+        "low"  : rendah (crispData),
+        "mid"  : sedang (crispData),
+        "high" : tinggi (crispData)
+    }
+
+def Inteference (fuzzyData):
+    return {
+        "Y":   (fuzzyData[0]["low"] and fuzzyData[1]["high"]) 
+            or (fuzzyData[0]["high"] and fuzzyData[1]["low"])
+            or (fuzzyData[0]["mid"] and fuzzyData[1]["mid"])
+            or (fuzzyData[0]["high"] and fuzzyData[1]["mid"])
+            or (fuzzyData[0]["high"] and fuzzyData[1]["high"]),
+        "T":   (fuzzyData[0]["low"] and fuzzyData[1]["low"]) 
+            or (fuzzyData[0]["low"] and fuzzyData[1]["mid"])
+            or (fuzzyData[0]["mid"] and fuzzyData[1]["low"])
+            or (fuzzyData[0]["mid"] and fuzzyData[1]["heigh"])
+    }
+
+def Defuzzification (fuzzyData):
+    pass
 
 if __name__ == '__main__':
     
     # initialize control dataset
     for i in range (0, 18):
-        testData.append ({
+        controlData.append ({
             'id': i,
             'writtenScore': dataset.cell (i + 2, 2).value,
             'InterviewScore': dataset.cell (i + 2, 3).value,
@@ -59,18 +86,23 @@ if __name__ == '__main__':
         })
 
     print ('control dataset : ')
-    pp.pprint (testData)
+    pp.pprint (controlData)
     
     # initialize teset dataset
-    for i in range (0, 18):
+    for i in range (0, 10):
         testData.append ({
             'id': i,
-            'writtenScore': dataset.cell (i + 2, 2).value,
-            'InterviewScore': dataset.cell (i + 2, 3).value,
+            'writtenScore': dataset.cell (i + 2, 7).value,
+            'InterviewScore': dataset.cell (i + 2, 8).value,
             'result': None
         })
 
     print ('\ntest dateset : ')
+
     pp.pprint (testData)
 
+    plt.plot([x for x in range (0, low + interval)], [sigmoidDown (x, 0, low + interval) for x in range (0, low + interval)])
+    plt.plot([x for x in range (low - interval, mid + interval)], [sigmoid (x, low - interval, mid + interval) for x in range (low - interval, mid + interval)])
+    plt.plot([x for x in range (mid - interval, 100)], [sigmoidUp(x, mid - interval, 100) for x in range(mid - interval, 100)])
+    plt.show()
 
